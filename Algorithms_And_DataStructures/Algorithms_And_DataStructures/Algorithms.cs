41e440f15ad579ab5f032f126fe9e8d317a3bb92
Aaron Riddle <saved.by.thetree@hotmail.com>
2020-05-16 14:02:56 -0400

    Pivot

---
+++ Algorithms_And_DataStructures/Algorithms_And_DataStructures/Algorithms.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_And_DataStructures
+{
+    public class Algorithms
+    {
+        public int pivot(int[] arr, int end, int start = 0)
+        {
+            end = end != 0 ? end : arr.Length + 1;
+            var pivot = arr[start]; //actual value
+            var swapIdx = start; // keep track of where we're putting the value we're going to move to
+
+            for (int i = start + 1; i < arr.Length; i++)
+            {
+                Debug.WriteLine($"swapIdx: {swapIdx}, i: {i}");
+                if (pivot > arr[i])
+                {
+                    swapIdx++;
+                    swap(arr, swapIdx, i);
+                }
+                printArray(arr);
+            }
+            swap(arr, start, swapIdx);
+
+            return swapIdx;
+        }
+
+        private void swap(int[] arr, int i, int j)
+        {
+            var temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        static void printArray(int[] arr)
+        {
+            Debug.WriteLine(string.Join(", ", arr));
+        }
+    }
+}
---
+++ Algorithms_And_DataStructures/Algorithms_And_DataStructures/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_And_DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var algo = new Algorithms();
+            int[] sample = new[] {4, 8, 2, 1, 5, 7, 6, 3};
+
+            var pivotIndex = algo.pivot(sample, sample.Length + 1, 0);
+            Console.WriteLine(pivotIndex);
+
+
+
+            Console.ReadKey();
+        }
+
+        static void printArray(int[] arr)
+        {
+            Console.WriteLine(string.Join(", ", arr));
+        }
+    }
+}
