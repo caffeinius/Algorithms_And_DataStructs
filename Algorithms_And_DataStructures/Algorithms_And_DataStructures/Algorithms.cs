1f6efb08df43ba802c067ea233b7e1bb63a17219
Aaron Riddle <saved.by.thetree@hotmail.com>
2020-05-17 14:22:55 -0400

    dang. the course I'm going through did this in javascript and it's much easier to manipulate things there. I'm going to nix this for now and come back.

--- Algorithms_And_DataStructures/Algorithms_And_DataStructures/Algorithms.cs
+++ Algorithms_And_DataStructures/Algorithms_And_DataStructures/Algorithms.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
@@ -11,6 +12,66 @@ namespace Algorithms_And_DataStructures
 {
     public class Algorithms
     {
+        public int[] RadixSort(int[] arr)
+        {
+            var mostDigitCount = MostDigits(arr);
+            for (int k = 0; k < mostDigitCount; k++)
+            {
+                //var digitBuckets = new int[10][];
+                //for (var i = 0; i < arr.Length; i++)
+                //{ 
+                //    digitBuckets[i] = new int[]{};
+                //    var digit = (int)GetDigit(arr[i], k);
+                //    digitBuckets[digit][i] = arr[i];
+                //}
+                //Array.Copy(digitBuckets, arr, digitBuckets.Length);
+
+                // create an array of arrays
+                var digitBuckets = new List<List<double>>(); // or double[]
+
+                //for each number in arr...
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    //get the digit in the place k
+                    var digit = GetDigit(arr[i], k);
+
+                    //add it to the digit bucket
+                    //digitBuckets.Insert(digit, ); 
+                }
+
+                //put the numbers back into arr in the order they were added to the digit bucket
+                
+            }
+            return arr;
+        }
+        public double GetDigit(int num, double place)
+        {
+            var baseNum = Math.Abs(num);
+            var power = Math.Pow(10.0, place);
+
+            var result = Math.Floor((baseNum / power) % 10);
+            return result;
+        }
+
+        private double DigitCount(int num)
+        {
+            if (num == 0) return 1;
+            return Math.Floor(Math.Log10(Math.Abs(num))) + 1;
+        }
+
+        public double MostDigits(int[] nums)
+        {
+            double mostDigits = 0;
+            foreach (var num in nums)
+            {
+                mostDigits = Math.Max(mostDigits, DigitCount(num));
+            }
+
+            Debug.WriteLine($"MostDigits: {mostDigits}");
+
+            return mostDigits;
+        }
+
         public int[] QuickSort(int[] arr, int left, int right)
         {
             //var pivotIndex = pivot(arr, 0, arr.Length + 1);
@@ -25,7 +86,7 @@ namespace Algorithms_And_DataStructures
 
             return arr;
         }
-        public int pivot(int[] arr, int start, int end)
+        private int pivot(int[] arr, int start, int end)
         {
             var pivot = arr[start]; //actual value
             var swapIdx = start; // keep track of where we're putting the value we're going to move to
--- Algorithms_And_DataStructures/Algorithms_And_DataStructures/Program.cs
+++ Algorithms_And_DataStructures/Algorithms_And_DataStructures/Program.cs
@@ -14,18 +14,17 @@ namespace Algorithms_And_DataStructures
             var algo = new Algorithms();
             int[] sample = new[] {4, 8, 2, 1, 5, 7, 6, 3};
 
-            //var pivotIndex = algo.pivot(sample, 0, sample.Length + 1);
-            //Console.WriteLine(pivotIndex);
+            //var quickSort = algo.QuickSort(sample, 0, sample.Length - 1);
+            //printArray(quickSort);
 
-            var quickSort = algo.QuickSort(sample, 0, sample.Length - 1);
-            printArray(quickSort);
+            printArray(algo.RadixSort(new[] {23, 234, 5467, 12, 2345, 9852}));
 
             Console.ReadKey();
         }
 
         static void printArray(int[] arr)
         {
-            Console.WriteLine(string.Join(", ", arr));
+            Debug.WriteLine(string.Join(", ", arr));
         }
     }
 }
