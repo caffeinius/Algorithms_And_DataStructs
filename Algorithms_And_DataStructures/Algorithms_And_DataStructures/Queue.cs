936494cf04ac4324fce0ee13928da5fe42f40f0d
Aaron Riddle <saved.by.thetree@hotmail.com>
2020-05-17 23:08:39 -0400

    Stacks and Queues

---
+++ Algorithms_And_DataStructures/Algorithms_And_DataStructures/Queue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_And_DataStructures
+{
+    public class Queue
+    {
+        public Node first;
+        public Node last;
+        public int size;
+
+        public Queue()
+        {
+            size = 0;
+        }
+
+        public int Enqueue(string val)
+        {
+            var newNode = new Node(val);
+            if (first is null)
+            {
+                first = newNode;
+                last = newNode;
+            }
+            else
+            {
+                last.next = newNode;
+                last = newNode;
+            }
+
+            Debug.WriteLine($"Size: {size}");
+            return size++;
+        }
+
+        public string Dequeue()
+        {
+            if (first is null) return null;
+            var temp = first;
+            if (first == last)
+            {
+                last = null;
+            }
+
+            first = first.next;
+            size--;
+
+            Debug.WriteLine($"Value: {first.val}");
+            return temp.val;
+        }
+    }
+}
---
+++ Algorithms_And_DataStructures/Algorithms_And_DataStructures/Stack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_And_DataStructures
+{
+    public class Stack
+    {
+        public Node first;
+        public Node last;
+        public int size;
+
+        public Stack()
+        {
+            size = 0;
+        }
+
+        public int Push(string val)
+        {
+            var newNode = new Node(val);
+            if (first == null)
+            {
+                first = newNode;
+                last = first;
+            }
+            else
+            {
+                var temp = first;
+                first = newNode;
+                first.next = temp;
+            }
+
+            size++;
+            Debug.WriteLine($"Size: {size}");
+            return size;
+        }
+
+        public string Pop()
+        {
+            if (first == null) return null;
+            var temp = first;
+            if (this.first == this.last)
+                this.last = null;
+
+            first = this.first.next;
+            size--;
+
+            Debug.WriteLine($"Value: {first.val}");
+            return temp.val;
+        }
+    }
+}
