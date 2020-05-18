e8b290bff1de7c7e51ae89ec0d3f52ab8341d715
Aaron Riddle <saved.by.thetree@hotmail.com>
2020-05-17 20:07:02 -0400

    init, push and pop

---
+++ Algorithms_And_DataStructures/Algorithms_And_DataStructures/DoublyLinkedList.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_And_DataStructures
+{
+    public class DoublyLinkedList
+    {
+        public int length;
+        public Node head;
+        public Node tail;
+
+        public DoublyLinkedList()
+        {
+            head = null;
+            tail = null;
+            length = 0;
+        }
+
+        public Node Pop()
+        {
+            if (head == null) return null;
+            var poppedNode = this.tail;
+            if (length == 1)
+            {
+                head = null;
+                tail = null;
+            }
+            else
+            {
+                tail = poppedNode.prev;
+                tail.next = null;
+                poppedNode.prev = null;
+            }
+
+            length--;
+            return poppedNode;
+        }
+
+        public DoublyLinkedList Push(string val)
+        {
+            Node newNode = new Node(val);
+            if (head == null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                tail.next = newNode;
+                newNode.prev = this.tail;
+                this.tail = newNode;
+            }
+
+            length++;
+            return this;
+        }
+
+        //private void printNode(Node n, string nodeName = null)
+        //{
+        //    Debug.WriteLineIf(string.IsNullOrEmpty(nodeName), $"{nodeName}::");
+        //    Debug.WriteLineIf(n.prev != null, $"prev val: {n.prev.val}");
+        //    Debug.WriteLine($"current val: {n.val}");
+        //    Debug.WriteLineIf(n.next != null, $"next val: {n.next.val}");
+        //}
+    }
+}
--- Algorithms_And_DataStructures/Algorithms_And_DataStructures/Program.cs
+++ Algorithms_And_DataStructures/Algorithms_And_DataStructures/Program.cs
@@ -11,18 +11,31 @@ namespace Algorithms_And_DataStructures
     {
         static void Main(string[] args)
         {
-           SinglyLinkedList ssl = new SinglyLinkedList();
-           ssl.Push("hello");
-           ssl.Push("staying");
-           ssl.Push("goodbye");
-           ssl.Insert(3, "Still here");
-           ssl.Traverse();
+            DoublyLinkedList dll = new DoublyLinkedList();
+            dll.Push("hello");
+            Debug.WriteLine(dll.head.val);
+            Debug.WriteLine(dll.tail.val);
+            dll.Push("good bye");
+            Debug.WriteLine(dll.head.val);
+            Debug.WriteLine(dll.tail.val);
+            dll.Pop();
+            //Debug.WriteLine(dll.Pop().val);
 
-           ssl.Reverse();
-           ssl.Traverse();
+
+            //SinglyLinkedList ssl = new SinglyLinkedList();
+            //ssl.Push("hello");
+            //ssl.Push("staying");
+            //ssl.Push("goodbye");
+            //ssl.Insert(3, "Still here");
+            //ssl.Traverse();
+
+            //ssl.Reverse();
+            //ssl.Traverse();
 
 
             Console.ReadKey();
+
+
         }
 
         static void printArray(int[] arr)
